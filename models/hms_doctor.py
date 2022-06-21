e31fc28d68e6051aaf171d1358bf9bbfe08b28d5
mohamadbakri <mohammed_bakri@hotmail.com>
2022-06-21 15:50:50 +0200

    If The pcr field is checked, the CR ratio field should be mandator

--- models/hms_doctor.py
+++ models/hms_doctor.py
@@ -4,9 +4,13 @@ from odoo import models, fields
 class HmsDoctor(models.Model):
     _name = "hms.doctor"
     _description = "Hms Doctors"
-    _rec_name = "first_name"
+    _rec_name = "full_name"
 
+    full_name = fields.Char(compute="get_full_name")
     first_name = fields.Char(required=True)
     last_name = fields.Char(required=True)
     image = fields.Binary()
 
+    def get_full_name(self):
+        for rec in self:
+            self.full_name=f"{rec.first_name} {rec.last_name}"
