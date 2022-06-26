76a94c6c287a808e8740e7875e60e5c09dfbb75b
mohamadbakri <mohammed_bakri@hotmail.com>
2022-06-26 15:01:04 +0200

    Create a patients report

--- __manifest__.py
+++ __manifest__.py
@@ -9,5 +9,7 @@
         'views/hms_doctor_views.xml',
         'views/hms_menus.xml',
         'views/res_partner_views.xml',
+        'reports/hms_templates.xml',
+        'reports/hms_reports.xml',
     ]
 }
